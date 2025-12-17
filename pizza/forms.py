97e4351280c6a32711281430e7bcbbb08f57ad76
Myat Ko Ko <coderinsider1@gmail.com>
2025-12-17 11:28:34 +0630

    Advanced Widgets

--- pizza/forms.py
+++ pizza/forms.py
@@ -1,21 +1,24 @@
 from django import forms
-from .models import Pizza
+from .models import Pizza, Size
 
-class PizzaForm(forms.Form):
-    topping1 = forms.CharField(label='Topping1', max_length=100)
-    topping2 = forms.CharField(label='Topping2',max_length=100)
-    #topping = forms.MultipleChoiceField(choices=[('pep', 'Pepproni'), ('cheese', 'Cheese'), ('olives', 'Olives')], widget=forms.CheckboxSelectMultiple)
-    CHOICES = (
-        ("small", "Small"),
-        ("medium", "Medium"),
-        ("large", "Large")
-    )
+# class PizzaForm(forms.Form):
+#     topping1 = forms.CharField(label='Topping1', max_length=100)
+#     topping2 = forms.CharField(label='Topping2',max_length=100)
+#     #topping = forms.MultipleChoiceField(choices=[('pep', 'Pepproni'), ('cheese', 'Cheese'), ('olives', 'Olives')], widget=forms.CheckboxSelectMultiple)
+#     CHOICES = (
+#         ("small", "Small"),
+#         ("medium", "Medium"),
+#         ("large", "Large")
+#     )
     
-    sizes    = forms.ChoiceField(label="Size", choices=CHOICES)
+#     sizes    = forms.ChoiceField(label="Size", choices=CHOICES)
 
 
-# class PizzaForm(forms.ModelForm):
-#     class Meta:
-#         model = Pizza
-#         fields = ['topping1', 'topping2', 'size']
-#         labels = {"topping1": "Topping 1", "topping2": "Topping 2"}
+class PizzaForm(forms.ModelForm):
+
+    size = forms.ModelChoiceField(queryset=Size.objects, empty_label=None, widget=forms.RadioSelect)
+    class Meta:
+        model = Pizza
+        fields = ['topping1', 'topping2', 'size']
+        labels = {"topping1": "Topping 1", "topping2": "Topping 2"}
+        # widgets = {'size': forms.CheckboxSelectMultiple}
