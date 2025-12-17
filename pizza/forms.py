8bf4d2c3052b2b7a449710430a38e6452d195019
Myat Ko Ko <coderinsider1@gmail.com>
2025-12-17 11:21:16 +0630

    Working with widget:

--- pizza/forms.py
+++ pizza/forms.py
@@ -1,20 +1,21 @@
 from django import forms
 from .models import Pizza
 
-# class PizzaForm(forms.Form):
-#     topping1 = forms.CharField(label='Topping1', max_length=100)
-#     topping2 = forms.CharField(label='Topping2',max_length=100)
-#     CHOICES = (
-#         ("small", "Small"),
-#         ("medium", "Medium"),
-#         ("large", "Large")
-#     )
+class PizzaForm(forms.Form):
+    # topping1 = forms.CharField(label='Topping1', max_length=100, widget=forms.Textarea)
+    # topping2 = forms.CharField(label='Topping2',max_length=100)
+    topping = forms.MultipleChoiceField(choices=[('pep', 'Pepproni'), ('cheese', 'Cheese'), ('olives', 'Olives')], widget=forms.CheckboxSelectMultiple)
+    CHOICES = (
+        ("small", "Small"),
+        ("medium", "Medium"),
+        ("large", "Large")
+    )
     
-#     sizes    = forms.ChoiceField(label="Size", choices=CHOICES)
+    sizes    = forms.ChoiceField(label="Size", choices=CHOICES)
 
 
-class PizzaForm(forms.ModelForm):
-    class Meta:
-        model = Pizza
-        fields = ['topping1', 'topping2', 'size']
-        labels = {"topping1": "Topping 1", "topping2": "Topping 2"}
+# class PizzaForm(forms.ModelForm):
+#     class Meta:
+#         model = Pizza
+#         fields = ['topping1', 'topping2', 'size']
+#         labels = {"topping1": "Topping 1", "topping2": "Topping 2"}
